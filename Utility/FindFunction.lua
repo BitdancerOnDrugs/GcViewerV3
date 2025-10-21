7ade5e22dad607fa7bca1dd886407d471ec2f521
Hyperion <weasely@soyboy.observer>
2025-10-21 20:17:54 +0100

    added closure info dump, return matching, proto & debug filters

--- Utility/FindFunction.lua
+++ Utility/FindFunction.lua
@@ -59,7 +59,6 @@ function FindFunc:CheckUpvalues(func, totalUpvalues, expected)
 
     return true
 end
-
 function FindFunc:CheckProtos(func, expectedProtoCount)
     assert(typeof(func) == "function", "First argument must be a function")
     assert(typeof(expectedProtoCount) == "number", "Second argument must be a number")
@@ -70,6 +69,24 @@ function FindFunc:CheckProtos(func, expectedProtoCount)
     end
 
     return #protos == expectedProtoCount
+end
+function FindFunc:CheckReturns(func, expected)
+    assert(typeof(func) == "function", "Argument must be a function")
+    local ok, result = pcall(func)
+    if not ok then
+        return false
+    end
+
+    if typeof(expected) == "table" then
+        for i, v in ipairs(expected) do
+            if result[i] ~= v then
+                return false
+            end
+        end
+        return true
+    else
+        return result == expected
+    end
 end
 function FindFunc:GcLookUp(val, callback, filter)
     for i, v in next, getgc(val and true or nil) do
@@ -131,6 +148,25 @@ function FindFunc:FindFunctionsByScript(script)
     end)
     return found
 end
+function FindFunc:FindFunctionsByReturn(expected)
+    local found = {}
+    self:GcLookUp(true, function(_, func)
+        if self:CheckReturns(func, expected) then
+            table.insert(found, func)
+        end
+    end)
+    return found
+end
+function FindFunc:FindFunctionsByNameChunkLine(nameOrChunk)
+    local found = {}
+    self:GcLookUp(true, function(_, func)
+        local info = debug.info(func, "nS")
+        if info and (info.name == nameOrChunk or info.short_src == nameOrChunk or info.source == nameOrChunk) then
+            table.insert(found, func)
+        end
+    end)
+    return found
+end
 function FindFunc:GetAllMatching(filterFunc)
     local found = {}
     self:GcLookUp(true, function(_, func)
@@ -139,6 +175,34 @@ function FindFunc:GetAllMatching(filterFunc)
         end
     end)
     return found
+end
+function FindFunc:GetClosureInfo(func)
+    if typeof(func) ~= "function" then
+        return {}
+    end
+
+    local info = {
+        constants = debug.getconstants(func),
+        upvalues = debug.getupvalues(func),
+        protos = debug.getprotos(func),
+        script = getscriptfromfunction and getscriptfromfunction(func) or nil,
+        debug = debug.info(func, "nSluf")
+    }
+
+    return info
+end
+function FindFunc:DumpFunctionInfo(func)
+    local data = self:GetClosureInfo(func)
+    print("Function:", tostring(func))
+    print("Script:", tostring(data.script))
+    print("Constants:", typeof(data.constants) == "table" and #data.constants or "n/a")
+    print("Upvalues:", typeof(data.upvalues) == "table" and #data.upvalues or "n/a")
+    print("Protos:", typeof(data.protos) == "table" and #data.protos or "n/a")
+    if data.debug then
+        print("Name:", data.debug.name)
+        print("Source:", data.debug.source)
+        print("Line:", data.debug.linedefined)
+    end
 end
 function FindFunc.getscriptfromthread(script)
     if not getscriptfromthread then
