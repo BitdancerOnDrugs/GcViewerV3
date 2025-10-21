f3f724b79a98d0384df865acd7b5a1a0fab262a7
Hyperion <weasely@soyboy.observer>
2025-10-21 19:20:21 +0100

    Added function finders by constants and upvalues, improve GcLookUp with filters
    - Added `FindFunctionsByConstants(expectedCount, expectedConstants)` to find GC functions by constants.
    - Added `FindFunctionsByUpvalues(expectedCount, expectedUpvalues)` to find GC functions by upvalues.
    - Enhanced `GcLookUp` to accept an optional filter function.
    - Removed duplicate definition of `GcLookUp`.

--- Utility/FindFunction.lua
+++ Utility/FindFunction.lua
@@ -15,7 +15,6 @@ function FindFunc:CheckConstants(func, totalConstants, expected)
 
     for index, expectedValue in next, expected do
         local actualValue = constants[index]
-
         if type(expectedValue) == "string" and expectedValue:match("^typeof:") then
             local expectedType = expectedValue:sub(8)
             if typeof(actualValue) ~= expectedType then
@@ -46,7 +45,6 @@ function FindFunc:CheckUpvalues(func, totalUpvalues, expected)
 
     for index, expectedValue in next, expected do
         local actualValue = count[index]
-
         if type(expectedValue) == "string" and expectedValue:match("^typeof:") then
             local expectedType = expectedValue:sub(8)
             if typeof(actualValue) ~= expectedType then
@@ -61,43 +59,60 @@ function FindFunc:CheckUpvalues(func, totalUpvalues, expected)
 
     return true
 end
-function FindFunc:GcLookUp(val, callback)
-    for i, v in next,getgc(val and true or nil) do
+function FindFunc:GcLookUp(val, callback, filter)
+    for i, v in next, getgc(val and true or nil) do
         if typeof(v) == "function" and islclosure(v) and not isexecutorclosure(v) then
-            if callback then
-                callback(i, v)
+            if filter then
+                if filter(v) then
+                    if callback then
+                        callback(i, v)
+                    else
+                        return v
+                    end
+                end
             else
-                warn("Found function:", v)
+                if callback then
+                    callback(i, v)
+                else
+                    return v
+                end
             end
         end
     end
 end
-function FindFunc.GcLookUp(val, callback)
-    for i, v in next, getgc(val and true or nil) do
-        if typeof(v) == "function" and islclosure(v) and not isexecutorclosure(v) then
-            if callback then
-                callback(i, v)
-            else
-                return v
-            end
+function FindFunc:FindFunctionsByConstants(expectedCount, expectedConstants)
+    local found = {}
+    self:GcLookUp(true, function(_, func)
+        if self:CheckConstants(func, expectedCount, expectedConstants) then
+            table.insert(found, func)
         end
-    end
+    end)
+    return found
+end
+function FindFunc:FindFunctionsByUpvalues(expectedCount, expectedUpvalues)
+    local found = {}
+    self:GcLookUp(true, function(_, func)
+        if self:CheckUpvalues(func, expectedCount, expectedUpvalues) then
+            table.insert(found, func)
+        end
+    end)
+    return found
 end
 function FindFunc.getscriptfromthread(script)
     if not getscriptfromthread then
         return "Executor doesnt not support getscriptfromthread"
     end
     for i,v in next, getreg() do 
-	    if typeof(v) == "thread" and getscriptfromthread(v) == script then
-	      return getscriptfromthread(v)
-	    end
+        if typeof(v) == "thread" and getscriptfromthread(v) == script then
+            return getscriptfromthread(v)
+        end
     end
 end
 function FindFunc.getscriptfromfunction(f)
     if not getscriptfromfunction then
-         return "Executor doesnt not support getscriptfromfunction"
+        return "Executor doesnt not support getscriptfromfunction"
     end
     return getscriptfromfunction(f)
 end
 
-return FindFunc      
+return FindFunc
