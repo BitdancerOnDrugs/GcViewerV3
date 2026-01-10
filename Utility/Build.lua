1ea45403c5607eba8f13e63e297722ce5784d8f3
Hyperion <weasely@soyboy.observer>
2026-01-10 10:38:01 +0000

    Update version and enhance log messages
    Updated version number and log messages for clarity.

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,11 +1,13 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "0.0.1"
+            return "0.2"
         elseif key == "__log" then
             return {
-                    "[/] Detection patching",
-                    "[/] MetaMethod invoking patched",
+                "[+] Added proper actor system",
+                "[+] Re-factor entire script",
+                "[+] Actor frame is alot more stable now",
+                "[+] Rewrote filtering, rewrote start ups, rewrote script decompile"
             }
         end
         return nil 
